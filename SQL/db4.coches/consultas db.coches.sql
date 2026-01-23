74273f52ab32fee4e4d528f767d070d13f3baf18
ana-arco <ana.arco.daw@gmail.com>
2026-01-23 17:25:01 +0000

    SQL : viernes 23 enero

---
+++ SQL/db4.coches/consultas db.coches.sql
@@ -0,0 +1,4 @@
+/*Consultas sencillas en la base de datos coches.*/
+/*1. Obtener todos los campos de todos los concesionarios.*/
+
+/*2. Obtener todos los campos de todos los clientes de  Madrid.*/
--- SQL/db4.coches/create database coches.sql
+++ SQL/db4.coches/create database coches.sql
@@ -1,31 +1,47 @@
-create database coches;
+/*drop database coches;*/
+create database if not exists coches;
 use coches;
 
 create table marcas (
-	cifm int(11),
+	cifm int,
     nombre varchar(15),
     ciudad varchar(20)
 );
 
 create table coches (
-	codcoche int(11),
+	codcoche int,
     nombre varchar(15),
-    modelo varchar(15)
+    modelo varchar(15),
+    cifm int
 );
 
 create table distribucion (
-	cantidad int(11)
+	cantidad int,
+    cifm int,
+    codcoche int
 );
 
 create table concesionario (
-	cifc int(11),
+	cifc int,
     nombre varchar(15),
     ciudad varchar(20)
 );
 
 create table ventas (
-	cifc int(11),
+	cifc int,
     dni varchar(9),
-    codcoche int(11),
+    codcoche int,
     color varchar(10)
 );
+
+/*constraints*/
+alter table marcas add constraint mar_cif_pk primary key (cifm);
+alter table coches add constraint coc_cod_pk primary key (codcoche);
+alter table coche add constraint coc_cif_fk foreign key (cifm) references marcas(coches);
+alter table concesionario add constraint con_cif_pk primary key (cifc);
+alter table clientes add constraint cli_dni_pk primary key (dni);
+alter table distribucion add constraint dis_cod_pk primary key (cifc, codcoche);
+alter table distribucion add constraint dis_cif_fk foreign key (cifc) references concesionario(cifc);
+alter table distribucion add constraint dis_cod_fk foreign key (codcoche) references coches(codcoche);
+alter table ventas add constraint ventas_pk primary key (cifc, codcoche, dni, color);
+alter table ventas add constraint ventas_fk primary key (cifc, codcoche, dni, color);
